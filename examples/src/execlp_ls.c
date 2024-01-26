111aa45e20d17dc413d168faf7e7bcfe0fee31a3
Hampus Jigehamn <hampus.jigehamn@gmail.com>
2024-01-26 13:15:25 +0100

    synk
    Co-authored-by: Richard Widén <Grimrath@users.noreply.github.com>

--- examples/src/execlp_ls.c
+++ examples/src/execlp_ls.c
@@ -2,7 +2,8 @@
 #include <stdio.h>  // perror()
 #include <stdlib.h> // EXIT_SUCCESS, EXIT_FAILURE
 
-int main(void) {
+int main(void)
+{
   execlp("ls", "ls", "-l", NULL);
   perror("Return from execlp() not expected");
   exit(EXIT_FAILURE);
--- mandatory/src/pipeline.c
+++ mandatory/src/pipeline.c
@@ -7,24 +7,36 @@
 #define WRITE 1
 #define CHILD 0
 #define ERROR -1
+#define CHILD_COUNT 2
+#define STDIN 0
+#define STDOUT 1
 
 void child_a(int fd[])
 {
-  // TODO: Add code here.
-
+  dup2(fd[WRITE], STDOUT);
   execlp("ls", "ls", "-F", "-1", NULL);
+
+  perror("Return from execlp() not expected");
+  exit(EXIT_FAILURE);
 }
 
 void child_b(int fd[])
 {
+  dup2(fd[READ], STDIN);
+  execlp("nl", "nl", NULL);
 
-  // TODO: Add code here.
+  perror("Return from execlp() not expected");
+  exit(EXIT_FAILURE);
 }
 
 int main(void)
 {
   int fd[2];
-  // TODO: create pipe
+  if (pipe(fd) == ERROR)
+  {
+    perror("unexpected failure of pipe()");
+    exit(EXIT_FAILURE);
+  }
 
   pid_t child_a_id;
   pid_t child_b_id;
@@ -32,33 +44,59 @@ int main(void)
   pid_t pid = fork();
   if (pid == ERROR)
   {
-    // TODO: Error
+    perror("unexpected failure of first call to fork()");
+    exit(EXIT_FAILURE);
   }
   else if (pid == CHILD)
   {
     child_a(fd);
-    exit(EXIT_SUCCESS);
   }
 
   child_a_id = pid;
+
   // Create second child
   pid = fork();
   if (pid == ERROR)
   {
-    // Error
+    perror("unexpected failure of second call to fork()");
+    exit(EXIT_FAILURE);
   }
   else if (pid == CHILD)
   {
     child_b(fd);
-    exit(EXIT_SUCCESS);
   }
 
   child_b_id = pid;
 
-  // TODO: fix
   int child_a_status = __INT_MAX__;
-  waitpid(child_a_id, &child_a_status);
-
   int child_b_status = __INT_MAX__;
-  waitpid(child_b_id, &child_b_status);
+
+  for (int i = 0; i < CHILD_COUNT; ++i)
+  {
+    int status = __INT_MAX__;
+    int child_pid = wait(&status);
+
+    if ((long)child_pid == child_a_id)
+    {
+      child_a_status = status;
+    }
+    else if ((long)child_pid == child_b_id)
+    {
+      child_b_status = status;
+    }
+    else if ((long)child_pid == ERROR)
+    {
+      perror("unexpected failure of call to wait()");
+      exit(EXIT_FAILURE);
+    }
+    else
+    {
+      perror("unexpected pid");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  // TODO: Kill child b, since nl waits for CTRL+D
+
+  return child_a_status == 0 && child_b_status == 0;
 }
