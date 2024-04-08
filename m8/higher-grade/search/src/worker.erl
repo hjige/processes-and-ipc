cf0434420d2975263482965a9695ae7e442b03f9
Hampus Jigehamn <hampus.jigehamn@gmail.com>
2024-04-08 11:20:42 +0200

    format

--- m8/higher-grade/search/src/worker.erl
+++ m8/higher-grade/search/src/worker.erl
@@ -5,32 +5,30 @@
 %% @doc Starts a worker process. The worker will make random guesses between
 %% `Min' and `Max'.
 
--spec start(Server, Master, Min, Max) -> Worker when
-      Server :: pid(), 
-      Master :: pid(),
-      Min :: number(), 
-      Max :: number(),
-      Worker :: pid().
-
+-spec start(Server, Master, Min, Max) -> Worker
+  when Server :: pid(),
+       Master :: pid(),
+       Min :: number(),
+       Max :: number(),
+       Worker :: pid().
 start(Server, Master, Min, Max) ->
-   spawn(fun() -> loop(Server, Master, Min, Max, 0) end).
-
+  spawn(fun() -> loop(Server, Master, Min, Max, 0) end).
 
 loop(Server, Master, Min, Max, Guesses) ->
-    process_flag(trap_exit, true),
-    Guess = utils:random(Min, Max),
-    Server ! {guess, Guess, self()},
-    master:log_guess(Master, Guess, Guesses, self()),
+  process_flag(trap_exit, true),
+  Guess = utils:random(Min, Max),
+  Server ! {guess, Guess, self()},
+  master:log_guess(Master, Guess, Guesses, self()),
 
-    receive
-        {right, Guess} ->
-            master:winner(Master, self()),
-            io:format("~p ~*.. B <== Found the winner ~n", [self(), utils:width(Max), Guess]),
-            ok;
-        {wrong, Guess} ->
-            io:format("~p ~*.. B~n", [self(), utils:width(Max), Guess]),
-            loop(Server, Master, Min, Max, Guesses + 1);
-        {'EXIT', _From, loser} ->
-          io:format("~p I lose :(~n", [self()]),
-          master:loser(Master, self())
-    end.
+  receive
+    {right, Guess} ->
+      io:format("~p ~*.. B <== Found the winner ~n", [self(), utils:width(Max), Guess]),
+      master:winner(Master, self()),
+      ok;
+    {wrong, Guess} ->
+      io:format("~p ~*.. B~n", [self(), utils:width(Max), Guess]),
+      loop(Server, Master, Min, Max, Guesses + 1);
+    {'EXIT', _From, loser} ->
+      io:format("~p I lose :(~n", [self()]),
+      master:loser(Master, self())
+  end.
